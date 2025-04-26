f42658c2582066b418e6155bb6f728b238c46e1f
caochenxu <1962662569@qq.com>
2025-04-26 16:47:53 +0800

    AI大模型接入

---
+++ src/main/java/com/cao/caoaiagent/CaoAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.cao.caoaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaoAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaoAiAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cao/caoaiagent/CaoAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cao.caoaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaoAiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
